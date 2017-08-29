22ebaa299ddc0825e11dd7482450d219688908a1
Charlyn Oyakhilome <charlynoyakhilome@gmail.com>
2017-08-29 19:14:02 +0100

    intro to CRUD assignment

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,21 @@
 class PostsController < ApplicationController
   def index
-  end
+    @posts = Post.all
+    @posts.each_with_index do |post, index|
+      if index % 5 == 0
+        post.update!(title: "SPAM")
+      end
+    end
 
   def show
+
   end
 
   def new
+
   end
 
   def edit
   end
+ end
 end
