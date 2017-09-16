94233d4acdb073a5627275222f9da5afadfddf3f
Charlyn Oyakhilome <charlynoyakhilome@gmail.com>
2017-09-17 00:14:41 +0100

    confirm user assignment

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   resources :topics do
   resources :posts, except: [:index]
   resources :sponsored_posts, except: [:index]
+
 end
 resources :users, only: [:new, :create]
 
+post 'users/confirm' => 'users#confirm'
   get 'about' => 'welcome#about'
   root 'welcome#index'
 end
