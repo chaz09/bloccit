36c89cb3ed897f1bf572e5f5bdc405b3419e2aae
Charlyn Oyakhilome <charlynoyakhilome@gmail.com>
2017-09-16 11:14:48 +0100

    22 Rails:Vlidations assignment done

--- app/models/sponsored_post.rb
+++ app/models/sponsored_post.rb
@@ -1,3 +1,6 @@
 class SponsoredPost < ActiveRecord::Base
-belongs_to :topic
+	belongs_to :topic
+	validates :title, length: { minimum: 5 }, presence: true
+	validates :body, length: { minimum: 15 }, presence: true
+	validates :topic, presence: true
 end
