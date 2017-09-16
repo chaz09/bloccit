4d0caaea22469d31319fa28190a3877e5aa8e91f
Charlyn Oyakhilome <charlynoyakhilome@gmail.com>
2017-09-16 20:52:00 +0100

    Rails Authentication - User model assignment

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
 
 before_save { self.email = email.downcase if email.present? }
+before_save :format_name
+
 validates :name, length: { minimum: 1, maximum: 100 }, presence: true
 validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
 validates :password, length: { minimum: 6 }, allow_blank: true
@@ -12,8 +14,15 @@ validates :email,
 
    has_secure_password
 
+def format_name
 
-
-
+if name
+  name_array = []
+  name.split.each do |name_part|
+    name_array << name_part.capitalize
+  end
+  self.name = name_array.join("")
+end
+end
 
 end
