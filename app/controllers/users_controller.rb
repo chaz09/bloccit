7c3e57eab93beade99c7a7ca752e34be3b94653b
Charlyn Oyakhilome <charlynoyakhilome@gmail.com>
2017-09-17 11:48:38 +0100

    25:rails authentication sign in assignment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,28 +5,36 @@ class UsersController < ApplicationController
 
    def confirm
    @user = User.new
+
    @user.name = params[:user][:name]
    @user.email = params[:user][:email]
    @user.password = params[:user][:password]
    @user.password_confirmation = params[:user][:password_confirmation]
 end
 
+def new
+     @user = User.new
+   end
 
-def create
-    @user = User.new
-    @user.name = params[:user][:name]
-    @user.email = params[:user][:email]
-    @user.password = params[:user][:password]
-    @user.password_confirmation = params[:user][:password_confirmation]
+   def create
+     @user = User.new
+     @user.name = params[:user][:name]
+     @user.email = params[:user][:email]
+     @user.password = params[:user][:password]
+     @user.password_confirmation = params[:user][:password_confirmation]
 
-    if @user.save
-      flash[:notice] = "Welcome to Bloccit #{@user.name}!"
-      create_session(@user)
-      redirect_to root_path
-    else
-      flash[:error] = "There was an error creating your account. Please try again."
-      render :new
-    end
-  end
-
-end
+     if @user.save
+       flash[:notice] = "Welcome to Bloccit #{@user.name}!"
+       create_session(@user)
+       redirect_to root_path
+     else
+       flash.now[:alert] = "There was an error creating your account. Please try again."
+       render :new
+     end
+   end
+   
+   def avatar_url(user)
+     gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+     "http://gravatar.com/avatar/#{gravatar_id}.png?s=48"
+   end
+ end
